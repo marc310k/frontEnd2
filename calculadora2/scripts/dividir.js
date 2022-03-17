e7a501204105f1c4a857b389ab2da43189b040c3
marc310k <marcelo.koskur@gmail.com>
2022-03-17 17:25:00 -0300

    tentativa 1

--- calculadora2/scripts/dividir.js
+++ calculadora2/scripts/dividir.js
@@ -1,5 +1,10 @@
 let dividir = (valor1, valor2) => {
+    if (valor1 === 0){
+        console.log("não se pode dividir por zero")
+    } if (valor2 === 0){
+        console.log("não se pode dividir por zero")
+    }
     return console.log(valor1/valor2);
 }
 
-dividir(12,6);
+dividir(0,6);
--- calculadora2/scripts/multiplicar.js
+++ calculadora2/scripts/multiplicar.js
@@ -1,5 +1,10 @@
 let multiplicar = (valor1, valor2) => {
+    if (valor1 === 0){
+        console.log("qualque número multiplicado por zero é igual a zero")
+    } if (valor2 === 0){
+        console.log("qualque número multiplicado por zero é igual a zero")
+    }
     return console.log(valor1*valor2);
 }
 
-multiplicar(12,6);
+multiplicar(6,0);
